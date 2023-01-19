caa085b3dac86107659f37e50e4bcbb5cb0ca3b4
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-19 08:30:10 +0100

    milestone 1

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('vue ok', Vue);
+
+const app = Vue.createApp({
+
+    data() {
+        return {
+            emailList: [],
+            numberOfEmail: 10
+        }
+    },
+
+    methods: {
+        
+        getEmailAddresses(numberOfEmail){
+            for(i=0; i < numberOfEmail; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( response => {
+                    const email = response.data.response;
+                    this.emailList.push(email);
+                })
+            }
+        },
+        
+
+    },
+
+    mounted() {
+        this.getEmailAddresses(this.numberOfEmail);
+    },
+        
+ });
+
+app.mount('#root');
